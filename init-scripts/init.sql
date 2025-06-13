8b1fe1ff144b566aafc00ce3d36dbb352320ff31
Marco Nahuel Ferrioli <ferriolimarconahuel@gmail.com>
2025-06-13 12:35:18 -0300

    Create tables for medical_appointments database

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -1,2 +1,51 @@
 --Create database medical_appointments
-CREATE DATABASE medical_appointments;
+CREATE DATABASE IF NOT EXISTS medical_appointments;
+
+--Use medical_appointments database
+USE medical_appointments;
+
+--Create table patient
+CREATE TABLE IF NOT EXISTS patient(
+    patient_id INT, 
+    name VARCHAR(250),
+    sex VARCHAR(50),
+    dob DATE,
+    insurance VARCHAR(250),
+    PRIMARY KEY(patient_id)
+);
+
+--Create table Slots
+CREATE TABLE IF NOT EXISTS slots(
+    slot_id INT,
+    appointment_date DATE,
+    appointment_time TIME,
+    is_available BOOLEAN,
+    PRIMARY KEY(slot_id)
+);
+
+--Create table status
+CREATE TABLE IF NOT EXISTS status(
+    status_id INT,
+    status_description VARCHAR(250),
+    PRIMARY KEY(status_id)
+);
+
+--Create table appointments
+CREATE TABLE appointments(
+    appointment_id INT,
+    slot_id INT,
+    scheduling_date DATE,
+    status_id INT, 
+    check_in_time TIME,
+    appointment_duration DECIMAL(4,1),
+    start_time TIME,
+    end_time TIME, 
+    waiting_time TIME,
+    patient_id INT,
+    PRIMARY KEY (appointment_id),
+    CONSTRAINT fk_slot FOREIGN KEY (slot_id) REFERENCES slots (slot_id),
+    CONSTRAINT fk_status FOREIGN KEY (status_id) REFERENCES status (status_id),
+    CONSTRAINT fk_patient FOREIGN KEY (patient_id) REFERENCES patient (patient_id)
+);
+
+
